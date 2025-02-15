9b69314dd693cf7fc3767086c08f223b68c49fb5
Aqib Ansari <aqibansari72a@gmail.com>
2025-02-15 18:31:35 +0530

    Test

--- copyWithin.js
+++
@@ -1,8 +0,0 @@
-Array.prototype.myCopy = function (indexAt, Start, End){
-	const sliced = this.slice(Start, End)
-	for (let i=0; i<sliced.length; i++){
-		console.log(`${indexAt} + ${i} = ${indexAt+i}`)
-		this[indexAt+i] = sliced[i]
-	}
-	return this
-}
--- fill.js
+++
@@ -1,6 +0,0 @@
-Array.prototype.myFill = function (value, Start=0, End=this.length){
-    for (let i = Start; i < End; i++){
-        this[i] = value
-    }
-    return this
-}
